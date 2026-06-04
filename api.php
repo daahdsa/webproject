2f67895b33d00866bb0b7e1ef5e9f2a5806b80b6
daahdsa <diana.saakyan003@gmail.com>
2026-06-04 17:53:56 +0300

    first commit

--- api.php
+++ api.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 header("Content-Type: application/json");
-$data = json_decode(file_get_contents("php://input"), true);
-var_dump(data);
+//$data = json_decode(file_get_contents("php://input"), true);
+
+var_dump(1);
